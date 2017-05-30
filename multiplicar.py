d88b02320d67c4072b49b4fca48b797586c2969a
solucioneswma <solucioneswma@gmail.com>
2017-05-30 13:05:24 -0500

    multiplica dos números en n procesos
    Ejemplo de implementacion
    $ mpiexec -n 5 python  calculadora/division.py 5 40000000000000000000
    da: 200000000000000000000.00

---
+++ multiplicar.py
@@ -0,0 +1,30 @@
+#multiplica un numero enorme entre un entero
+import numpy
+from mpi4py import MPI
+from decimal import *
+
+import sys
+
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+n = Decimal(sys.argv[1])/(size - 1)
+n1 = Decimal(sys.argv[2])
+
+
+if (rank != 0):
+	resta = n*n1
+	comm.send(resta, dest=0, tag=11)
+else:
+	cont = 0;
+	for i in range(1, size):
+		total = comm.recv(source=i, tag=11)
+		cont = cont + total
+	print cont
+
+n = comm.bcast(n, root=0)
+n1 = comm.bcast(n1, root=0)
+
+# un aporte de Nelson Caraballo Ayala
